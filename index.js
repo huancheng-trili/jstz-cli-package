6320fac9e7bcfd21ecd67946b42eabbd04dfa634
Huan-Cheng Chang <huancheng.chang@trili.tech>
2025-01-20 14:30:24 +0000

    update

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const execFile = require('child_process').execFile;
-const child = execFile('sh', ['/home/pi/Desktop/twitter/test'], (error, stdout, stderr) => {
+const child = execFile('sh', ['a.sh'], (error, stdout, stderr) => {
   if (error) {
     throw error;
   }
