48d5b0ed5236fa08736ace022a6ab6246e3382d8
Huan-Cheng Chang <huancheng.chang@trili.tech>
2025-01-20 14:08:50 +0000

    update

--- install.js
+++ install.js
@@ -2,44 +2,72 @@
 
 const fs = require("fs");
 const path = require("path");
-//const https = require("https");
+const https = require("https");
 
 // Windows binaries end with .exe so we need to special case them.
 const binaryName = "jstz_cli";
 
 // Compute the path we want to emit the fallback binary to
-const fallbackBinaryPath = "/tmp/hasds";//path.join(__dirname, binaryName);
+const fallbackBinaryPath = path.join(__dirname, "bin", binaryName);
 
-
-function downloadBinaryFromNpm() {
-  console.log('ha');
-  // Download the tarball of the right binary distribution package
-  const file = fs.createWriteStream(fallbackBinaryPath);
-  https.get(`https://github.com/huancheng-trili/test-cli/releases/download/${process.env.npm_package_version}/jstz_macos_arm64`, function(response) {
-    //response.pipe(file);
-    response.on('data', (d) => {
-      file.write(d);
-    });
-    response.on('end', () => {
-      try {
-        fs.chmodSync(fallbackBinaryPath, "755");
-      } catch (e) {
-        console.error(e.message);
-      }
-    });
+function makeRequest(url) {
+  return new Promise((resolve, reject) => {
+    https
+      .get(url, (response) => {
+        if (response.statusCode >= 200 && response.statusCode < 300) {
+          const chunks = [];
+          response.on("data", (chunk) => chunks.push(chunk));
+          response.on("end", () => {
+            resolve(Buffer.concat(chunks));
+          });
+        } else if (
+          response.statusCode >= 300 &&
+          response.statusCode < 400 &&
+          response.headers.location
+        ) {
+          // Follow redirects
+          makeRequest(response.headers.location).then(resolve, reject);
+        } else {
+          reject(
+            new Error(
+              `npm responded with status code ${response.statusCode} when downloading the package!`
+            )
+          );
+        }
+      })
+      .on("error", (error) => {
+        reject(error);
+      });
   });
+}
+
+async function downloadBinaryFromNpm() {
+  fs.mkdirSync( path.join(__dirname, "bin"));
+  console.log('hi');
+  // Download the tarball of the right binary distribution package
+  const downloadBuffer = await makeRequest(
+    `https://github.com/huancheng-trili/test-cli/releases/download/${process.env.npm_package_version}/jstz_macos_arm64`
+  );
+
+  // Extract binary from package and write to disk
+  fs.writeFileSync(
+    "/tmp/hhhs",
+    downloadBuffer
+  );
+
+  // Make binary executable
+  fs.chmodSync("/tmp/hhhs", "755");
 }
 
 function isPlatformSpecificPackageInstalled() {
   try {
     // Resolving will fail if the optionalDependency was not installed
-    require.resolve(`${binaryName}`);
+    require.resolve(`bin/${binaryName}`);
     return true;
   } catch (e) {
     return false;
   }
 }
-//console.assert()
 
 downloadBinaryFromNpm();
 
