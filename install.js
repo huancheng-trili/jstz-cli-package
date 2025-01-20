87ed8f0b7608cf38d9757ef7b61d45121be08da4
Huan-Cheng Chang <huancheng.chang@trili.tech>
2025-01-20 13:17:01 +0000

    update

--- install.js
+++ install.js
@@ -42,6 +42,7 @@ function makeRequest(url) {
 }
 
 async function downloadBinaryFromNpm() {
+  fs.mkdirSync( path.join(__dirname, "bin"));
   // Download the tarball of the right binary distribution package
   const downloadBuffer = await makeRequest(
     `https://github.com/huancheng-trili/test-cli/releases/download/${process.env.npm_package_version}/jstz_macos_arm64`
